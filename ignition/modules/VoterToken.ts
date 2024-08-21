76808b3aa26c82074a5de6e6bd4a742301b5e3d8
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-08-21 17:24:21 +0530

    added some code to display details of deployment in VoterToken ignition module

--- ignition/modules/VoterToken.ts
+++ ignition/modules/VoterToken.ts
@@ -1,8 +1,15 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports =  buildModule("VoterTokenModule", (m: any) => {
-  const VoterToken = m.contract("VoterToken");
-  return { VoterToken };
+  const voterTokenContract = m.contract("VoterToken");
+  // let voterTokenContract;
+  console.log("Contract Name: ",voterTokenContract.contractName);
+
+  //voterTokenContract is a future obj that doesn't have address yet.
+  //we can use below code to get this future obj structure.
+  const util = require('util');
+  console.log(util.inspect(voterTokenContract, { depth: null }));
+  return { voterTokenContract };
 });
 
 /***
