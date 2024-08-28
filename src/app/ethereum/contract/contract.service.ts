f643869b5d067d3d4c380189f95d72f123955cd4
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-08-28 21:59:35 +0530

    some code cleanup;

--- src/app/ethereum/contract/contract.service.ts
+++ src/app/ethereum/contract/contract.service.ts
@@ -35,7 +35,6 @@ export class ContractService {
 
   setupEthereumConnection = async() => {
     if (this.walletService.getWalletConnection() == null) {
-
       // If MetaMask is not installed, we use the default provider,
       // which is backed by a variety of third-party services (such
       // as INFURA). They do not have private keys installed,
@@ -44,7 +43,6 @@ export class ContractService {
       await this.connectWithDefaultProvider();
 
     } else {
-
       // Connect to the MetaMask EIP-1193 object. This is a standard
       // protocol that allows Ethers access to make all read-only
       // requests through MetaMask.
@@ -54,9 +52,6 @@ export class ContractService {
     // If no %%url%% is provided, it connects to the default
     // http://localhost:8545, which most nodes use.
     // await this.connectWithJsonRpcProvider();
-
-    // this.contract = this.getContract("0x5FbDB2315678afecb367f032d93F642f64180aa3", 
-    //   ReusableVotingContractabi);
   }
 
   async connectWithGivenProvider(
@@ -83,14 +78,8 @@ export class ContractService {
   }
 
   connectWithDefaultProvider = async () => {
-
     await this.connectWithGivenProvider(
       ethers.getDefaultProvider(),addressForContractDeployment,ReusableVotingContractabi);
-    // this.provider = ethers.getDefaultProvider();
-    // this.network = await this.provider.getNetwork();
-    // this.signer = await this.provider.getSigner();
-
-
     console.log("Default Provider");
   }
 
@@ -98,46 +87,13 @@ export class ContractService {
     await this.connectWithGivenProvider(
       new ethers.BrowserProvider(this.walletService.getWalletConnection()),
       addressForContractDeployment,ReusableVotingContractabi);
-    // this.provider = new ethers.BrowserProvider(this.walletService.getWalletConnection());
-    // this.network = await this.provider.getNetwork();
     console.log("Browser Provider");
-
-    // It also provides an opportunity to request access to write
-    // operations, which will be performed by the private key
-    // that MetaMask manages for the user.
-    // this.signer = await this.provider.getSigner();
-    // console.log("Signer address:", await this.signer.getAddress());
-
-    // return this.signer;
   }
 
   connectWithJsonRpcProvider = async () => {
     await this.connectWithGivenProvider(
       new ethers.JsonRpcProvider(),
       addressForContractDeployment,ReusableVotingContractabi);
-    // this.provider = new ethers.JsonRpcProvider();
-    // this.network = await this.provider.getNetwork();
     console.log("JsonRpc Provider");
-
-    // this.provider.getNetwork().then((network: { name: any; }) => {
-    //   console.log('Connected to network:', network.name);
-    // });
-
-    // Get write access as an account by getting the signer
-    // this.signer = await this.provider.getSigner();
-    // console.log("Signer address:", await this.signer.getAddress());
   }
-
-  getContract = async (address: string, abi: ethers.Interface | ethers.InterfaceAbi) => {
-    this.contract = new Contract(address, abi, this.provider)
-    console.log("Contract Name ",await this.contract.name())
-    console.log("Contract Address ",await this.contract.getAddress())
-    console.log("Owner Address ",await this.contract.owner())
-
-    // return this.contract;
-  }
-
-  // getLoggedInAccount() : Observable<any>{
-  //   return from(this.signer.getAddress());
-  // }
 }
