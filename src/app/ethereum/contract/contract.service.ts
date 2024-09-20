da09969fb79b83595ef8cf2039e54d28d528b7ac
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-09-20 11:58:19 +0530

    moved all wallet connection related info into wallet service; added code to detect account change in metamask or any other connected wallet;

--- src/app/ethereum/contract/contract.service.ts
+++ src/app/ethereum/contract/contract.service.ts
@@ -18,102 +18,57 @@ const addressForContractDeployment = "0x5FbDB2315678afecb367f032d93F642f64180aa3
 })
 export class ContractService {
 
-  public provider: any;
-  public signer: any;
-  public signerAddress: any;
-  public network: any;
-  public chainId: any;
+  // public provider: any;
+  // public signer: any;
+  // public signerAddress: any;
+  // public network: any;
+  // public chainId: any;
   public contract: any;
   public contractOwnerAddress: any;
 
   constructor(
-    private walletService: WalletService,
-    private commonData: CommonDataService
+    private ws: WalletService,
+    private cd: CommonDataService
   ) {
-    this.provider = null;
-    this.signer = null;
-    this.network = null;
+    // this.provider = null;
+    // this.signer = null;
+    // this.network = null;
+    this.ws.getSignerAccount();
     this.contract = null;
 
     this.setupEthereumConnection();
   }
 
   setupEthereumConnection = async() => {
-    if (this.walletService.getWalletConnection() == null) {
-      // If MetaMask is not installed, we use the default provider,
-      // which is backed by a variety of third-party services (such
-      // as INFURA). They do not have private keys installed,
-      // so they only have read-only access
-      console.log("MetaMask not installed; using read-only defaults")
-      await this.connectWithDefaultProvider();
 
-    } else {
-      // Connect to the MetaMask EIP-1193 object. This is a standard
-      // protocol that allows Ethers access to make all read-only
-      // requests through MetaMask.
-      await this.connectWithBrowserProvider();
-    }
+    await this.connectWithGivenProvider(
+      this.ws.getProvider(),addressForContractDeployment,ReusableVotingContractabi);
+
+    // if (this.ws.getWalletConnection() == null) {
+    //   // If MetaMask is not installed, we use the default provider,
+    //   // which is backed by a variety of third-party services (such
+    //   // as INFURA). They do not have private keys installed,
+    //   // so they only have read-only access
+    //   console.log("MetaMask not installed; using read-only defaults")
+    //   await this.connectWithDefaultProvider();
+
+    // } else {
+    //   // Connect to the MetaMask EIP-1193 object. This is a standard
+    //   // protocol that allows Ethers access to make all read-only
+    //   // requests through MetaMask.
+    //   await this.connectWithBrowserProvider();
+    // }
 
     // If no %%url%% is provided, it connects to the default
     // http://localhost:8545, which most nodes use.
     // await this.connectWithJsonRpcProvider();
+
+    this.ws.currAcc$.subscribe(currAccChanged => {
+      this.ws.getSignerAccount();
+    })
   }
 
-  // async presetup(provider: any){
-  //   this.provider = provider;
-  //   this.network = await this.provider.getNetwork();
-  //   this.signer = await this.provider.getSigner();
-  //   this.signerAddress = await this.signer.getAddress();
-  //   this.chainId = this.network.chainId;
-  //   console.log(this.chainId)
-  // }
-
-  async connectWithGivenProvider(
-      provider: any, 
-      address: string, 
-      abi: ethers.Interface | ethers.InterfaceAbi
-  ) {
-    this.provider = provider;
-    this.network = await this.provider.getNetwork();
-    this.signer = await this.provider.getSigner();
-    this.signerAddress = await this.signer.getAddress();
-    // this.chainId = this.network.chainId;
-
-    this.contract = new Contract(address, abi, this.provider);
-
-
-    //store data in common data service
-    this.commonData.setData(PROVIDER, this.provider);
-    this.commonData.setData(NETWORK, this.network);
-    this.commonData.setData(SIGNER, this.signer);
-    this.commonData.setData(SIGNER_ADDRESS, await this.signer.getAddress());
-    
-    //store data in common data service
-    this.commonData.setData(PROVIDER, this.provider);
-    this.commonData.setData(NETWORK, this.network);
-    this.commonData.setData(SIGNER, this.signer);
-    this.commonData.setData(SIGNER_ADDRESS, await this.signer.getAddress());
-
-    console.log("Chain Id", this.chainId);
-    // if(this.chainId.charAt(this.chainId.length-1)==='n'){
-    //   this.chainId = this.chainId.slice(0,this.chainId.length-1);
-    // }
-
-    await this.fetchAndSaveAllData();
-
-    // provider.on('accountsChanged', (accounts: any) => {
-    //   console.log('Accounts changed:', accounts);
-    // });
-
-
-    // console.log("Provider", this.network);
-    // console.log("Chain Id", this.network.chainId);
-    // console.log("Signer address:", await this.signer.getAddress());
-    // console.log("Contract Name ",await this.contract.name());
-    // console.log("Contract Address ",await this.contract.getAddress());
-    // console.log("Owner Address ",await this.contract.owner());
-  }
-
+  
   connectWithDefaultProvider = async () => {
     await this.connectWithGivenProvider(
       ethers.getDefaultProvider(),addressForContractDeployment,ReusableVotingContractabi);
@@ -122,7 +77,7 @@ export class ContractService {
 
   connectWithBrowserProvider = async () => {
     await this.connectWithGivenProvider(
-      new ethers.BrowserProvider(this.walletService.getWalletConnection()),
+      new ethers.BrowserProvider(this.ws.getWalletConnection()),
       addressForContractDeployment,ReusableVotingContractabi);
     console.log("Browser Provider");
   }
@@ -134,21 +89,55 @@ export class ContractService {
     console.log("JsonRpc Provider");
   }
 
+  async connectWithGivenProvider(
+      provider: any, 
+      address: string, 
+      abi: ethers.Interface | ethers.InterfaceAbi
+  ) {
+    // this.provider = provider;
+    // this.network = await this.provider.getNetwork();
+    // this.signer = await this.provider.getSigner();
+    // this.signerAddress = await this.signer.getAddress();
+    // this.chainId = this.network.chainId;
+
+    await this.setupAllConnectionDetails();
+
+    this.contract = new Contract(address, abi, provider);
+
+    // console.log("Chain Id", this.chainId);
+
+    await this.fetchAndSaveAllData();
+  }
+
+  async setupAllConnectionDetails(){
+    
+    // this.network = await this.provider.getNetwork();
+    // this.signer = await this.provider.getSigner();
+    // this.signerAddress = await this.signer.getAddress();
+    // this.chainId = this.network.chainId;
+
+    // this.contract = new Contract(address, abi, this.provider);
+  }
+
+
   async fetchAndSaveAllData(){
     //store data in common data service
-    this.commonData.setData(PROVIDER, this.provider);
+    // this.commonData.setData(PROVIDER, this.provider);
 
-    this.commonData.setData(NETWORK, this.network);
-    this.commonData.setData(CHAIN_ID, this.chainId);
+    // this.commonData.setData(NETWORK, this.network);
+    // this.commonData.setData(CHAIN_ID, this.chainId);
 
-    this.commonData.setData(SIGNER, this.signer);
-    this.commonData.setData(SIGNER_ADDRESS, await this.signer.getAddress());
+    // this.commonData.setData(SIGNER, this.signer);
+    // this.commonData.setData(SIGNER_ADDRESS, await this.signer.getAddress());
 
-    this.commonData.setData(CONTRACT, this.contract);
-    this.commonData.setData(CONTRACT_ADDRESS, await this.contract.getAddress());
-    this.commonData.setData(CONTRACT_OWNER, await this.contract.owner());
+    this.cd.setData(CONTRACT, this.contract);
+    this.cd.setData(CONTRACT_ADDRESS, await this.contract.getAddress());
+    this.cd.setData(CONTRACT_OWNER, await this.contract.owner());
 
-    this.commonData.setData(VOTING_EVENTS_LIST, await this.contract.getAllVotingEvents());
+    this.cd.setData(VOTING_EVENTS_LIST, await this.contract.getAllVotingEvents());
+    this.cd.getAllData().subscribe(data => {
+      console.log("STATE: ",data);
+    })
   }
 
   /***********************************************************************/
@@ -170,13 +159,13 @@ export class ContractService {
     const eventIdNo: BigNumberish = eventId;
     console.log("stopping eventIdNo ", eventIdNo);
 
-    const currentNonce = await this.provider.getTransactionCount(this.signerAddress);
-    const pendingNonce = await this.provider.getTransactionCount(this.signerAddress, 'pending');
+    const currentNonce = await this.ws.getProvider().getTransactionCount(this.ws.getSignerAddress());
+    const pendingNonce = await this.ws.getProvider().getTransactionCount(this.ws.getSignerAddress(), 'pending');
     console.log('Current nonce:', currentNonce, 'Pending: ', pendingNonce);
     // this.clearStuckTransactions(pendingNonce);
     try {
         this.contract
-            .connect(this.signer)
+            .connect(this.ws.getSigner())
               //TODO: clear stuck nonce to remove explicit currentNonce
             .endVoting(eventIdNo,{nonce:currentNonce})
             .then((success: any) => {
@@ -194,28 +183,28 @@ export class ContractService {
     }
 }
 
-  async clearStuckTransactions(nonce: number){
-    try {
-      // Create a transaction to cancel the pending one
-      const tx = {
-          to: await this.signer.getAddress(), // Send to your own address
-          value: ethers.parseUnits('0', 'wei'), // Zero value to cancel
-          nonce: nonce, // Use the nonce of the transaction you want to cancel
-          gasLimit: 21000, // Basic gas limit for a simple transaction
-          gasPrice: ethers.parseUnits('1000', 'gwei') // Higher gas price to ensure it is mined
-      };
+  // async clearStuckTransactions(nonce: number){
+  //   try {
+  //     // Create a transaction to cancel the pending one
+  //     const tx = {
+  //         to: await this.signer.getAddress(), // Send to your own address
+  //         value: ethers.parseUnits('0', 'wei'), // Zero value to cancel
+  //         nonce: nonce, // Use the nonce of the transaction you want to cancel
+  //         gasLimit: 21000, // Basic gas limit for a simple transaction
+  //         gasPrice: ethers.parseUnits('1000', 'gwei') // Higher gas price to ensure it is mined
+  //     };
 
-      // Send the cancellation transaction
-      const txResponse = await this.signer.sendTransaction(tx);
-      console.log('Cancel transaction hash:', txResponse.hash);
+  //     // Send the cancellation transaction
+  //     const txResponse = await this.signer.sendTransaction(tx);
+  //     console.log('Cancel transaction hash:', txResponse.hash);
 
-      // Wait for the transaction to be mined
-      const txReceipt = await txResponse.wait();
-      console.log('Cancel transaction mined:', txReceipt.transactionHash);
-  } catch (error) {
-      console.error('Error canceling transaction:', error);
-  }
-  }
+  //     // Wait for the transaction to be mined
+  //     const txReceipt = await txResponse.wait();
+  //     console.log('Cancel transaction mined:', txReceipt.transactionHash);
+  // } catch (error) {
+  //     console.error('Error canceling transaction:', error);
+  // }
+  // }
 
   /***********************************************************************/
 
--- src/app/ethereum/wallet/wallet.service.ts
+++ src/app/ethereum/wallet/wallet.service.ts
@@ -1,15 +1,43 @@
 import { Injectable } from '@angular/core';
+import { ethers, JsonRpcSigner } from 'ethers';
+import { BehaviorSubject } from 'rxjs';
+import { NETWORK, PROVIDER, SIGNER, SIGNER_ADDRESS } from '../../common-data/common-data.keys';
+import { CommonDataService } from '../../common-data/common-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
-
   public ethereum;
 
-  constructor() {
+  private provider: ethers.AbstractProvider;
+  public signer: any;
+  public signerAddress: any;
+  public network: any;
+  public chainId: any;
+
+  private currAccSubject = new BehaviorSubject<any>({});
+  currAcc$ = this.currAccSubject.asObservable();
+
+  constructor(private cd: CommonDataService) {
     const {ethereum} = <any>window
     this.ethereum = ethereum
+    if (this.ethereum) {
+      // Connect to the MetaMask EIP-1193 object. This is a standard
+      // protocol that allows Ethers access to make all read-only
+      // requests through MetaMask.
+      this.provider = new ethers.BrowserProvider(this.ethereum);
+      this.setupListeners();
+    } else {
+      // If MetaMask is not installed, we use the default provider,
+      // which is backed by a variety of third-party services (such
+      // as INFURA). They do not have private keys installed,
+      // so they only have read-only access
+      console.log("MetaMask not installed; using read-only defaults")
+      this.provider = ethers.getDefaultProvider()
+      console.error('Ethereum provider not found');
+    }
+    this.cd.setData(PROVIDER, this.provider);
   }
 
   public connectWallet = async () => {
@@ -33,6 +61,7 @@ export class WalletService {
     }
   }
 
+  // this is used in contract service to connect
   public getWalletConnection(){
     try{
       if(!this.ethereum) return alert("Please install meta mask ")
@@ -42,4 +71,48 @@ export class WalletService {
       throw new Error("No ethereum object found");
     }
   }
+
+  /********************************************************************* */
+
+  //check change in wallet account
+
+  private setupListeners() {
+    this.ethereum.on('accountsChanged', (accounts: string[]) => {
+      if (accounts.length === 0) {
+        this.currAccSubject.next(null);
+      } else {
+        this.currAccSubject.next(accounts[0]);
+      }
+    });
+
+    // Optionally, listen for network changes
+    this.ethereum.on('networkChanged', (networkId: number) => {
+      console.log('Network changed:', networkId);
+    });
+
+    // Optionally, listen for chain changes
+    this.ethereum.on('chainChanged', (chainId: number) => {
+      console.log('Chain changed:', chainId);
+    });
+  }
+
+  async getSignerAccount(): Promise<JsonRpcSigner> {
+    this.signer = await (this.provider as ethers.BrowserProvider).getSigner();
+    this.cd.setData(SIGNER, this.signer);
+
+    this.network = await this.provider.getNetwork();
+    this.cd.setData(NETWORK, this.network);
+
+    this.signerAddress = await this.signer.getAddress();
+    this.cd.setData(SIGNER_ADDRESS, await this.signer.getAddress());
+
+    this.chainId = this.network.chainId;
+    
+    return this.signer;
+  }
+
+  getProvider(){return this.provider;}
+  getSigner(){return this.signer;}
+  getSignerAddress(){return this.signerAddress;}
+  
 }
