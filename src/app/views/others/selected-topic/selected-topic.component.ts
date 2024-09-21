3f0ea1f4fcf1426f366e44c25775044890362ffe
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2024-09-21 18:05:16 +0530

    code cleanup;

--- src/app/views/others/selected-topic/selected-topic.component.ts
+++ src/app/views/others/selected-topic/selected-topic.component.ts
@@ -19,21 +19,14 @@ export class SelectedTopicComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedVotingEvent'] && !changes['selectedVotingEvent'].firstChange) {
-      // Property has changed (not initial binding)
-      console.log('selectedVotingEvent has changed:', changes['selectedVotingEvent'].currentValue, changes['selectedVotingEvent'].previousValue);
       if(this.selectedVotingEvent && !this.selectedVotingEvent[2]){
         for (var option of this.selectedVotingEvent[1]) {
           this.voteData.optionsList.push(option[0]);
           this.voteData.votesPerOption.push(Number(option[1]));
         }
-        console.log("this.voteData 1", this.voteData);
         this.cd.setData(RESULT_OPTION_LIST,this.voteData.optionsList);
         this.cd.setData(RESULT_VOTE_PER_OPTION, this.voteData.votesPerOption);
       }
-    }
-    if (changes['selectedEventId'] && !changes['selectedEventId'].firstChange) {
-      // Property has changed (not initial binding)
-      console.log('selectedEventId has changed:', changes['selectedEventId'].currentValue, changes['selectedEventId'].previousValue);
     }
   }
 
--- src/app/views/owner/existing-topic/existing-topic.component.ts
+++ src/app/views/owner/existing-topic/existing-topic.component.ts
@@ -20,54 +20,33 @@ export class ExistingTopicComponent implements OnChanges {
 
   constructor(
     private contractService: ContractService,
-    private cd: CommonDataService,
-    private ref: ChangeDetectorRef
+    private cd: CommonDataService
   ){}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedVotingEvent'] && !changes['selectedVotingEvent'].firstChange) {
-      // Property has changed (not initial binding)
-      console.log('selectedVotingEvent has changed:', changes['selectedVotingEvent'].currentValue, changes['selectedVotingEvent'].previousValue);
       if(this.selectedVotingEvent && !this.selectedVotingEvent[2]){
         for (var option of this.selectedVotingEvent[1]) {
           this.voteData.optionsList.push(option[0]);
           this.voteData.votesPerOption.push(Number(option[1]));
         }
-        console.log("this.voteData 1", this.voteData);
         this.cd.setData(RESULT_OPTION_LIST,this.voteData.optionsList);
         this.cd.setData(RESULT_VOTE_PER_OPTION, this.voteData.votesPerOption);
       }
-    }
-    if (changes['selectedEventId'] && !changes['selectedEventId'].firstChange) {
-      // Property has changed (not initial binding)
-      console.log('selectedEventId has changed:', changes['selectedEventId'].currentValue, changes['selectedEventId'].previousValue);
     }
   }
 
   stopVoting(){
     this.contractService.stopVoting(<any>this.selectedEventId);
-    //build data for piechart
     if(this.selectedVotingEvent && !this.selectedVotingEvent[2]){
-      //build data for pie chart
       for (var option of this.selectedVotingEvent[1]) {
         this.voteData.optionsList.push(option[0]);
-        this.voteData.votesPerOption.push(option[1]);
+        this.voteData.votesPerOption.push(Number(option[1]));
       }
-      console.log("this.voteData 2", this.voteData);
       this.cd.setData(RESULT_OPTION_LIST,this.voteData.optionsList);
       this.cd.setData(RESULT_VOTE_PER_OPTION, this.voteData.votesPerOption);
-      // this.selectedVotingEvent[1].for(
-      //   (option:any) => {
-      //     this.voteData.optionsList.push(option[0]);
-      //     this.voteData.votesPerOption.push(option[1]);
-      //   }
-      // );
-      console.log(this.voteData);
-      this.ref.markForCheck();
     }
   }
-
-
 
   async getAllEvents(){
     await this.contractService.fetchAndSaveAllData();
